463d17e53273af2f71d49b329dbd3da34d4d8a96
thecompliancecafe-cmd <thecompliancecafe@gmail.com>
2026-01-27 13:05:33 +0530

    Update init_treasury.sh

--- scripts/init_treasury.sh
+++ scripts/init_treasury.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+set -e
 source .env
 
-SIG1="0xSIGNER1"
-SIG2="0xSIGNER2"
-SIG3="0xSIGNER3"
+echo "Creating Treasury..."
+
+GAS_COIN=$(sui client gas --json | jq -r '.[0].id')
 
 sui client call \
   --package $PACKAGE_ID \
   --module treasury \
-  --function create_multisig_treasury \
-  --args $SIG1 $SIG2 $SIG3 2 \
-  --gas-budget 50000000
+  --function create \
+  --type-args 0x2::sui::SUI \
+  --args $GAS_COIN \
+  --gas-budget 50000000 \
+  --json > treasury.json
+
+TREASURY_ID=$(jq -r '.objectChanges[] | select(.objectType | contains("Treasury")) | .objectId' treasury.json)
+
+echo "TREASURY_ID=$TREASURY_ID" >> .env
