94ef9112f0c203ac214b03b3a0b573cf83c4f572
thecompliancecafe-cmd <thecompliancecafe@gmail.com>
2026-01-27 13:04:59 +0530

    Update init_oracle.sh

--- scripts/init_oracle.sh
+++ scripts/init_oracle.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+set -e
 source .env
 
-ORACLE_PUBKEY="0xAI_ORACLE_PUBKEY"
+echo "Initializing Oracle Registry..."
 
 sui client call \
   --package $PACKAGE_ID \
   --module ai_oracle \
-  --function register_oracle \
-  --args $ORACLE_PUBKEY \
-  --gas-budget 50000000
+  --function init \
+  --gas-budget 50000000 \
+  --json > oracle.json
+
+ORACLE_REGISTRY_ID=$(jq -r '.objectChanges[] | select(.objectType | contains("OracleRegistry")) | .objectId' oracle.json)
+ORACLE_ADMIN_CAP=$(jq -r '.objectChanges[] | select(.objectType | contains("OracleAdminCap")) | .objectId' oracle.json)
+
+echo "ORACLE_REGISTRY_ID=$ORACLE_REGISTRY_ID" >> .env
+echo "ORACLE_ADMIN_CAP=$ORACLE_ADMIN_CAP" >> .env
